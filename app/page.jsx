bc277b8f55a5f6c49fa9a5a934d4989e469e3809
ruspian <ruspianntb@gmail.com>
2025-12-21 13:52:32 +0800

    menambahkan navbar

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center font-sans dark:bg-black">
+      <h1>Hello Word!</h1>
+    </div>
+  );
+}
